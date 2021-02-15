f4e174c6b338f0e89faa4bf4f194a39f4d7264e6
Anja Seeba <ahoi@achtsam.media>
2021-02-15 15:26:32 +0100

    Install npm, react + eslint plugin and storybook for react

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import AppHeader from './components/AppHeader'
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+ReactDOM.render(
+  <AppHeader title="Hello world" />,
+  document.querySelector('#root')
+)
